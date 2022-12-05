94fac2d074549b6c4e9d9743009f6cb549b9d7de
Dare Blessing <dareblessing12@gmail.com>
2022-12-04 21:48:46 -0800

    hello

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"6-print_matrix_integer.py" [New File]                                              0,0-1         All
+
